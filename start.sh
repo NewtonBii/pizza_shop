cdc85c23917ca262247faea24fa1393d31f0b1ff
Newton Bii <biinewton382@gmail.com>
2018-01-03 14:26:46 +0300

    remove cached files for gitignore file.

--- start.sh
+++
@@ -1,7 +0,0 @@
-export SECRET_KEY='this is real'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://newton:newton@localhost/new_pitch'
-export MAIL_USERNAME ='biinewtondev@yahoo.com'
-export MAIL_PASSWORD ='Moringa@123'
-export DATABASE_URL='postgresql+psycopg2://newton:newton@localhost/new_pitch'
-
-python3.6 manage.py server
